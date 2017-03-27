db8612dfffefa69da1e70739c2163f96d0d01626
JTB <jtbdevelopment@gmail.com>
2017-03-27 18:55:01 -0400

    progress on sign-in

--- ui-a2bs4/src/app/core-ui-bs/sign-in/sign-in.component.ts
+++ ui-a2bs4/src/app/core-ui-bs/sign-in/sign-in.component.ts
@@ -1,26 +1,23 @@
 import {Component} from '@angular/core';
 import {CookieService} from 'angular2-cookie/services/cookies.service';
-import {Http, Response} from '@angular/http';
+import {Http} from '@angular/http';
 @Component({
     selector: 'sign-in',
     template: require('./sign-in.component.html'),
 })
 export class SignInComponent {
-    public message: string;
-    public csrf: string = '';
+    public message: string = '';
     public showManual: boolean = false;
     public showFacebook: boolean = false;
+
     constructor(private _cookieService: CookieService, private http: Http) {
-        this.message = 'Getting API';
-        //  TODO - fix me and not working anyway
-        this.http.get('/api/social/apis').toPromise().then(response => {
-            this.message = 'Got API';
-            console.log(response);
-            console.log(JSON.stringify(response.json().data));
-            this.csrf = this._cookieService.get('XSRF-TOKEN');
-            console.log(this.csrf);
-        }).catch(error => {
-            console.error(error);
-        });
+        //  TODO - not right
+        this.showFacebook = true;
+        this.showManual = true;
+    }
+
+    fbLogin(): void {
+        //  TODO - not right
+        console.log('fb login');
     }
 }
--- ui-a2bs4/src/app/routes.ts
+++ ui-a2bs4/src/app/routes.ts
@@ -1,10 +1,11 @@
 import {Component} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {MainComponent} from './main';
+import {SignInComponent} from './core-ui-bs/sign-in/sign-in.component';
 
 @Component({
   selector: 'fountain-root',
-  template: '<router-outlet></router-outlet>'
+  template: '<router-outlet></router-outlet><navigation-bar></navigation-bar>'
 })
 export class RootComponent {}
 
@@ -12,6 +13,10 @@ export const routes: Routes = [
   {
     path: '',
     component: MainComponent
+  },
+  {
+    path: 'signin',
+    component: SignInComponent
   }
 ];
 
