43965220803730d0bd2f6c7c1b98a7695d96616e
JTB <jtbdevelopment@gmail.com>
2017-04-10 14:31:47 -0400

    starting to add game update handling

--- ui-a2bs4/src/app/core-ui/messagebus/messagebus.service.ts
+++ ui-a2bs4/src/app/core-ui/messagebus/messagebus.service.ts
@@ -1,9 +1,17 @@
 import {Injectable} from '@angular/core';
 import {Player} from '../player/player.model';
 import {Subject, BehaviorSubject} from 'rxjs';
+import {Game} from '../games/game.model';
 
 @Injectable()
 export class MessageBusService {
     connectionStatus: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+
+    // In general, for internal use
+    // Use the feed off player service, which handles logged in vs simulated player
     playerUpdates: Subject<Player> = new Subject<Player>();
+
+    // In general, for internal use
+    // Use the feed off game cache which handles race conditions on updates
+    gameUpdates: Subject<Game> = new Subject<Game>();
 }
