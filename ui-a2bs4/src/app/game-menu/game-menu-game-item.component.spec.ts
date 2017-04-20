ce7ec910a387eaf64506452ee6033e05445d652a
JTB <jtbdevelopment@gmail.com>
2017-04-20 13:46:15 -0400

    test for game-item

---
+++ ui-a2bs4/src/app/game-menu/game-menu-game-item.component.spec.ts
@@ -0,0 +1,43 @@
+import {TestBed, async} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {GameMenuGameItemComponent} from './game-menu-game-item.component';
+import {MapKeysPipe} from '../core-ui/utils/map-keys.pipe';
+import {Game} from '../core-ui/games/game.model';
+
+
+describe('Component:  game menu game item component', () => {
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                RouterTestingModule
+            ],
+            declarations: [
+                GameMenuGameItemComponent,
+                MapKeysPipe
+            ],
+        });
+        TestBed.compileComponents();
+    }));
+
+    it('displays game', () => {
+        const fixture = TestBed.createComponent(GameMenuGameItemComponent);
+        fixture.componentInstance.game = new Game({
+            id: '1234',
+            gamePhase: 'Playing',
+            players: {'md51': 'p1', 'md52': 'p2'},
+            playerImages: {'md51': '', 'md52': ''}
+        });
+        fixture.detectChanges();
+        const link = fixture.nativeElement;
+        expect(link.querySelector('a')).not.toBeNull();
+        expect(link.querySelectorAll('img').length).toEqual(2);
+        expect(link.querySelector('span')).not.toBeNull();
+        expect(link.querySelector('span').textContent).toEqual('TODO');
+    });
+
+    it('default describe game function just returns todo', () => {
+        const fixture = TestBed.createComponent(GameMenuGameItemComponent);
+        expect(fixture.componentInstance.describeGame()).toEqual('TODO');
+    });
+
+});
