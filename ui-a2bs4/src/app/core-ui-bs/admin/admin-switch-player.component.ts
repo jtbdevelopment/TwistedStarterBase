7176a23a82d52cf7e6bd287c1ae345be81e2cf5a
JTB <jtbdevelopment@gmail.com>
2017-04-29 20:51:52 -0400

    working on switch user

--- ui-a2bs4/src/app/core-ui-bs/admin/admin-switch-player.component.ts
+++ ui-a2bs4/src/app/core-ui-bs/admin/admin-switch-player.component.ts
@@ -1,5 +1,7 @@
 import {Component} from '@angular/core';
 import {Player} from '../../core-ui/player/player.model';
+import {PlayerService} from '../../core-ui/player/player.service';
+import {Http} from '@angular/http';
 @Component({
     selector: 'admin-switch-player',
     template: require('./admin-switch-player.component.html'),
@@ -12,21 +14,59 @@ export class AdminSwitchPlayerComponent {
 
     public pageSize: number = 20;
     public totalPlayers: number = 0;
-    public currentPage: number = 0;
+    public currentPage: number = 1;
 
-    public revertToNormal(): void {
+    private player: Player;
+    private loggedInPlayer: Player;
 
-    }
-
-    public refreshUsers(): void {
-
+    constructor(private playerService: PlayerService, private http: Http) {
+        this.playerService.player.subscribe(p => {
+            this.player = p;
+            this.computeRevert();
+        });
+        this.playerService.loggedInPlayer.subscribe(p => {
+            this.loggedInPlayer = p;
+            this.computeRevert();
+        });
+        this.refreshUsers();
     }
 
     public switchToPlayer(id: string): void {
+        this.playerService.simulateUser(id);
+    }
 
+    public revertToNormal(): void {
+        this.playerService.simulateUser(this.loggedInPlayer.id);
+    }
+
+    public refreshUsers(): void {
+        let pageParams = '?pageSize=' + this.pageSize +
+            '&page=' + (this.currentPage - 1) +
+            //  TODO - encode
+            '&like=' + this.searchText;
+        this.http.get('/api/player/admin/playersLike/' + pageParams)
+            .map(response => response.json())
+            .subscribe(json => {
+                    this.totalPlayers = json.totalElements;
+                    // controller.numberOfPages = response.totalPages;
+                    let newPlayers = [];
+                    json.content.forEach(p => {
+                        newPlayers.push(new Player(p));
+                    });
+                    this.players = newPlayers;
+                    this.currentPage = json.number + 1;
+                }
+            );
     }
 
     public changePage(): void {
+        this.refreshUsers();
+    }
 
+    private computeRevert(): void {
+        this.revertEnabled = (this.player && this.loggedInPlayer && this.player.id !== this.loggedInPlayer.id);
+        this.revertText = this.revertEnabled ?
+            'You are simulating another player.' :
+            'You are yourself.';
     }
 }
