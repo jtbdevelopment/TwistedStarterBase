20e632ba87e008fad3a2964686d3f3472bbce9e4
JTB <jtbdevelopment@gmail.com>
2017-11-28 19:27:26 -0500

    added to maven buld

--- ui-a2bs4/conf/webpack-dist.conf.js
+++ ui-a2bs4/conf/webpack-dist.conf.js
@@ -41,6 +41,18 @@ module.exports = {
                 loaders: [
                     'html-loader'
                 ]
+            },
+            {
+                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: "url-loader?limit=10000&minetype=application/font-woff"
+            },
+            {
+                test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: "file-loader"
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                loader: "url-loader?limit=50000&name=[path][name].[ext]"
             }
         ]
     },
