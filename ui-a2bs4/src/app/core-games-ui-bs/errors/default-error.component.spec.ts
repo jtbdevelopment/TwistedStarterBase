7a29d76ce163acf26faddb4b0952be79259f7c2c
JTB <jtbdevelopment@gmail.com>
2018-01-14 14:36:33 -0500

    general error listener for session/http errors

--- ui-a2bs4/src/app/core-games-ui-bs/errors/default-error.component.spec.ts
+++ ui-a2bs4/src/app/core-games-ui-bs/errors/default-error.component.spec.ts
@@ -1,26 +1,35 @@
 import {TestBed} from '@angular/core/testing';
 import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
 import {DefaultErrorComponent} from './default-error.component';
+import {PlayerService} from '../../core-games-ui/player/player.service';
 
 class MockModal {
     dismiss = jasmine.createSpy('dismiss');
 }
 
+class MockPlayerService {
+    forceLogout = jasmine.createSpy('forceLogout');
+}
+
 describe('Component:  default error component', () => {
     let fixture;
     let modal;
+    let playerService;
+
     beforeEach(() => {
         TestBed.configureTestingModule({
             declarations: [
                 DefaultErrorComponent,
             ],
             providers: [
-                {provide: NgbActiveModal, useClass: MockModal}
+                {provide: NgbActiveModal, useClass: MockModal},
+                {provide: PlayerService, useClass: MockPlayerService}
             ],
         });
         TestBed.compileComponents();
         fixture = TestBed.createComponent(DefaultErrorComponent);
         modal = TestBed.get(NgbActiveModal);
+        playerService = TestBed.get(PlayerService) as MockPlayerService;
         fixture.detectChanges();
     });
 
@@ -32,5 +41,6 @@ describe('Component:  default error component', () => {
         expect(modal.dismiss).not.toHaveBeenCalled();
         fixture.nativeElement.querySelector('.close-button').click();
         expect(modal.dismiss).toHaveBeenCalled();
+        expect(playerService.forceLogout).toHaveBeenCalled();
     });
 });
--- ui-a2bs4/src/app/core-games-ui-bs/errors/default-error.component.ts
+++ ui-a2bs4/src/app/core-games-ui-bs/errors/default-error.component.ts
@@ -1,12 +1,17 @@
 import {Component} from '@angular/core';
 import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
+import {PlayerService} from '../../core-games-ui/player/player.service';
 
 @Component({
     selector: 'ngbd-modal-content',
     template: require('./default-error.component.html')
 })
 export class DefaultErrorComponent {
-    constructor(public activeModal: NgbActiveModal) {
+    constructor(private activeModal: NgbActiveModal, private playerService: PlayerService) {
     }
 
+    public dismissAndForceLogout() {
+        this.activeModal.dismiss();
+        this.playerService.forceLogout();
+    }
 }
