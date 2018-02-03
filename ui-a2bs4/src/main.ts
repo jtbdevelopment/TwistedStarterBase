05742f6ebc2181ac1f8af80f997176e876d238b0
JTB Development <jtbdevelopment@gmail.com>
2018-02-03 12:33:06 -0500

    fixing styles

--- ui-a2bs4/src/main.ts
+++ ui-a2bs4/src/main.ts
@@ -4,12 +4,6 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-import './index.scss';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'font-awesome/css/font-awesome.css';
-import 'primeng/resources/themes/bootstrap/theme.css';
-import 'primeng/resources/primeng.css';
-
 if (environment.production) {
   enableProdMode();
 }
