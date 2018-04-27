45eeb3a2c3d152cdab82aa09a71f796495fdc982
JTB <jtbdevelopment@gmail.com>
2018-04-27 06:40:30 -0400

    fix logback

--- game/src/main/java/com/jtbdevelopment/TwistedStarterBase/dao/GameRepository.java
+++ game/src/main/java/com/jtbdevelopment/TwistedStarterBase/dao/GameRepository.java
@@ -3,10 +3,12 @@ package com.jtbdevelopment.TwistedStarterBase.dao;
 import com.jtbdevelopment.TwistedStarterBase.state.GameFeature;
 import com.jtbdevelopment.TwistedStarterBase.state.TSBGame;
 import com.jtbdevelopment.games.mongo.dao.AbstractMongoMultiPlayerGameRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * Date: 7/13/16
  * Time: 7:13 PM
  */
+@Repository
 public interface GameRepository extends AbstractMongoMultiPlayerGameRepository<GameFeature, TSBGame> {
 }
